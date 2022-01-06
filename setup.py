2e4b3a21a8e8a93efb5194d6102b1adda4d8c4e2
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-01-06 12:31:28 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AirStationCli",
+    version="0.1",
+    description="BUFFALO AirStation Command Line Interface",
+    url="https://github.com/trsqxyz/pig",
+    packages=find_packages(),
+)
